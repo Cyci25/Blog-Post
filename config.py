3153f17151ef6a1be70b715dc63fc8d408cebed7
Cyci25 <c2muriithi@gmail.com>
2018-11-19 09:10:22 +0300

    Update on main folder

--- config.py
+++ config.py
@@ -1,16 +1,5 @@
 import os
-class Config:
-    '''
-    General configuration parent class
-    '''
-    # # pass
-    # SOURCE_NEWS_URL='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
 
-    # NEWS_API_SOURCE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
-    # # CATEGORIES_URL='https://newsapi.org/v2/everything?q={}&sortBy=relevancy&apiKey={}'
-
-    # NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
-    # SECRET_KEY=os.environ.get('SECRET_KEY')
 
 class ProdConfig(Config):
     '''
